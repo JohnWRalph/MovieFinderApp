2936284f9d9f44a727ede7948ebc7724a9896eaa
JohnWRalph <John.Ralph.Git@gmail.com>
2023-06-08 12:37:01 -0500

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  define: {global: 'window','process.env.ANCHOR_BROWSER': true,}, 
+  resolve: {
+    alias: {
+      process: "process/browser"
+    }
+  
+  },
+
+  
 })
